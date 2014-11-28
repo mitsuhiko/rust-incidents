67ca1e8e72d07d151d45e07a349250cc5b8e0840
Armin Ronacher <armin.ronacher@active-4.com>
2014-11-28 23:06:59 +0100

    Built documentation

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['incidents'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;E: <a class='trait' href='incidents/trait.Error.html' title='incidents::Error'>Error</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a>&lt;E&gt; for <a class='struct' href='incidents/struct.Failure.html' title='incidents::Failure'>Failure</a>&lt;E&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
